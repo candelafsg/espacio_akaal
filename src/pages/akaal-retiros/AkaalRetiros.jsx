dea20a028a07dca80ee7c3489cb0592cc305aa97
Candela94 <candelafsg@alumnos.cei.es>
2026-02-13 08:43:15 +0100

    feat(retiros)

--- src/pages/akaal-retiros/AkaalRetiros.jsx
+++ src/pages/akaal-retiros/AkaalRetiros.jsx
@@ -1,8 +1,25 @@
-
+import './retiros.css'
 
 const AkaalRetiros = () => {
+
+
     return ( 
+
         <>
+
+        <section className="retiros-portada">
+            <img src="https://res.cloudinary.com/dhwd1b4be/image/upload/v1770913737/Frame_50_2_raea1m.png" alt="portada" className="retiros-portada-img" />
+
+
+            <div className="retiros-info">
+                <h1 className="retiros-titulo">Retiro Solsticio</h1>
+
+                <div className="retiros-info-icon">
+                    <div className="info">19 - 21 Junio</div>
+                    <div className="info">Calma, Gilet</div>
+                </div>
+            </div>
+        </section>
      
         </>
      );
