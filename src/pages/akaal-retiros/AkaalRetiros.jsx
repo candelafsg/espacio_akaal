bed672a6badce5142c34657a2d5b363b0b111b6f
Candela94 <candelafsg@alumnos.cei.es>
2026-02-15 18:13:05 +0100

    style (no animaciones)

--- src/pages/akaal-retiros/AkaalRetiros.jsx
+++ src/pages/akaal-retiros/AkaalRetiros.jsx
@@ -1,7 +1,7 @@
 import './retiros.css'
 
 const AkaalRetiros = () => {
-    return ( 
+    return (
         <>
             <section className="retiros-portada">
                 <img src="https://res.cloudinary.com/dhwd1b4be/image/upload/v1770913737/Frame_50_2_raea1m.png" alt="portada" className="retiros-portada-img" />
@@ -21,8 +21,8 @@ const AkaalRetiros = () => {
                 </div>
 
                 <div className="retiro-resumen">
-                    <p className="retiro-texto">Una experiencia transformadora diseñada para reconectar con tu esencia durante el solsticio.</p> 
-                    <p className="retiro-texto">Tres días de pausa consciente entre las montañas de la Sierra Calderona.</p> 
+                    <p className="retiro-texto">Una experiencia transformadora diseñada para reconectar con tu esencia durante el solsticio.</p>
+                    <p className="retiro-texto">Tres días de pausa consciente entre las montañas de la Sierra Calderona.</p>
                 </div>
 
                 <div className="retiro-eleme">
@@ -30,45 +30,48 @@ const AkaalRetiros = () => {
                         <div className="line"></div>
                         <p className="retiro-text">INCLUIDO</p>
                     </div>
-                    
+
                     <ul className="retiro-ul">
                         <li className="retiro-li">
-                            <img src="/img/home.png" alt="home" className="img-incluye"/>
+                            <img src="/img/home.png" alt="home" className="img-incluye" />
                             <p className="retiro-p">Alojamiento</p>
                         </li>
                         <li className="retiro-li">
-                            <img src="/img/yoga.png" alt="yoga" className="img-incluye"/>
+                            <img src="/img/yoga.png" alt="yoga" className="img-incluye" />
                             <p className="retiro-p">Yoga y Meditación</p>
                         </li>
                         <li className="retiro-li">
-                            <img src="/img/gong.png" alt="gong" className="img-incluye"/>
+                            <img src="/img/gong.png" alt="gong" className="img-incluye" />
                             <p className="retiro-p">Terapia de sonido</p>
                         </li>
                         <li className="retiro-li">
-                            <img src="/img/nature.png" alt="nature" className="img-incluye"/>
+                            <img src="/img/nature.png" alt="nature" className="img-incluye" />
                             <p className="retiro-p">Inmersión en la naturaleza</p>
                         </li>
                     </ul>
                 </div>
 
-               
 
 
-             
-<div className="inmersion-info-precio">
+
+
+                <div className="inmersion-info-precio">
                     <div className="inmersion-info">
-                    <p className="inmersion-texto">INMERSIÓN COMPLETA</p>
-                    <h3 className="inmersion-precio">Desde 320€</h3>
+                        <p className="inmersion-texto">INMERSIÓN COMPLETA</p>
+                        <h3 className="inmersion-precio">Desde 320€</h3>
                     </div>
 
                     <div className="inmersion-info">
-                    <p className="inmersion-texto">PLAZAS LIMITADAS</p>
-                    
+                        <p className="inmersion-texto">PLAZAS LIMITADAS</p>
+                         <p className="inmersion-texto" style={{fontWeight:'700', textAlign:'end'}}>ANA y MARÍA</p>
+
                     </div>
-</div>
+                </div>
                 <div className="capa">
                     <img src="/img/capa.png" alt="simbolo" className="svg" />
                 </div>
+
+                
             </section>
         </>
     );
