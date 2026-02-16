9b8e4e9ae0a5fe30f6a87311ecf9ad9827ac813d
Candela94 <candelafsg@alumnos.cei.es>
2026-02-16 19:22:44 +0100

    feat (Whatsapp link): funciona con su numero

--- src/components/whatsapp-link/WhatsappLink.jsx
+++ src/components/whatsapp-link/WhatsappLink.jsx
@@ -3,10 +3,9 @@
 import React from "react";
 import { Button } from "../buttons/Button";
 
-const WhatsAppLink = ({ message, children, ...props }) => {
-  const phone = "34654068208"; // número fijo de la empresa
-  const encodedMessage = encodeURIComponent(message);
-  const whatsappLink = `https://wa.me/${phone}?text=${encodedMessage}`;
+const WhatsAppLink = ({ message = "", children, ...props }) => {
+  const phone = "34622502066"; // número fijo de la empresa
+  const whatsappLink = message ? `https://wa.me/${phone}?text=${encodeURIComponent(message)}` : `https://wa.me/${phone}`;
  
  
  
