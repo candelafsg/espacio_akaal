61908ed8af31baa2250029e730910f0e5f475bb7
Candela94 <candelafsg@alumnos.cei.es>
2026-03-02 12:49:57 +0100

    whatsapp

--- src/components/whatsapp-link/WhatsappLink.jsx
+++ src/components/whatsapp-link/WhatsappLink.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Button } from "../buttons/Button";
 
 const WhatsAppLink = ({ message = "", children, ...props }) => {
-  const phone = "34622502066"; // número fijo de la empresa
+  const phone = "34614218764"; // número fijo de la empresa
   const whatsappLink = message ? `https://wa.me/${phone}?text=${encodeURIComponent(message)}` : `https://wa.me/${phone}`;
  
  
